396274130a6235cfd39df674a5bac0cba5b4f3ea
HeJiaPeng2020 <2826474227@qq.com>
2021-01-19 18:03:08 +0800

    insert Bootstrap project stated!

---
+++ src/test/java/open/jp/bootstrap/BootstrapApplicationTests.java
@@ -0,0 +1,15 @@
+package open.jp.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
